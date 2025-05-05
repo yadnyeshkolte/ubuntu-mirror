36165eef01f27c06b451dcea0887221b8a8b5482
yadnyeshkolte <yadnyeshkolte@gmail.com>
2025-05-05 12:37:52 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/not-found";
+import NotFound from "./pages/not-found";
 import HomePage from "./pages/HomePage";
 
 function Router() {
